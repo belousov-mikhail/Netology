fc344a4bdf59942ceecbd46bd1f913f6a48f404b
TAG-748\Mikhail Belousov <belousoff2006@gmail.com>
2017-03-30 14:33:16 +0800

    minor improvements

--- Netology32/vk_friends_analys.py
+++ Netology32/vk_friends_analys.py
@@ -6,6 +6,7 @@ Created on Wed Mar 29 23:34:35 2017
 """
 import vk
 import networkx as nx
+import os
 
 
 def getFriendsList (vkapi, user_id):
@@ -17,17 +18,11 @@ def getFriendsList (vkapi, user_id):
     return friends
 
 
-def addToGlobal(global_dict, to_add):
-    for el in to_add:
-        if el[0] not in global_dict.keys():
-            global_dict[el[0]] = (el[1], el[2])
-    return(global_dict)
-
-
 def friendsToGraph(graph, start_node, friends):
     edges_to_add = [(start_node, el) for el in friends]
     graph.add_edges_from(edges_to_add)
     return graph
+
 
 def mostPopular(graph, n):
     degree_dict = dict (graph.degree_iter())
@@ -36,11 +31,16 @@ def mostPopular(graph, n):
     return most_popular
 
 
-def printTop (global_dict, most_popular):
+def printTop (most_popular):
     print ('Список из {} человек с максимальным количеством пересечений'.format(len(most_popular)))
     for el in most_popular:
-        id = el[0]
-        print ("{0} {1} связан(а) с {2} человек, id={3}".format(global_dict[id][0], global_dict[id][1], el[1], id))
+        print ("{0} {1} связан(а) с {2} человек, id={3}".format(el[0][1], el[0][2], el[1], el[0][0]))
+
+
+def saveGraph(graph):
+    export_to = 'vk_graph.gexf'
+    whole_file_name = os.path.join (os.getcwd(), export_to)
+    nx.write_gexf(graph, whole_file_name)
 
 
 def main():
@@ -49,21 +49,20 @@ def main():
     HOST_ID = 4118181
     host_data = vkapi.users.get(user_id=HOST_ID, fields='domain')[0]
     host = (HOST_ID, host_data['last_name'], host_data['first_name'])
-    global_dict = {}
-    global_dict[host[0]] = (host[1], host[2])
     g = nx.Graph()
     host_friends = getFriendsList(vkapi, HOST_ID)
-    global_dict = addToGlobal(global_dict, host_friends)
-    g = friendsToGraph(g, host[0], host_friends)
+    g = friendsToGraph(g, host, host_friends)
     for el in host_friends:
         more_friends = getFriendsList(vkapi, el[0])
-        global_dict = addToGlobal(global_dict, more_friends)
-        g = friendsToGraph(g, el[0], more_friends)
-    print (nx.info(g))
-    limit = int (input ("Какое количество самых популярных друзей вывести?\n>>"))
-    most_popular = mostPopular(g, 50)
-    printTop(global_dict, most_popular)
+        g = friendsToGraph(g, el, more_friends)
+    saveGraph(g)
+    limit = int(input("Какое количество самых популярных друзей вывести?\n>>"))
+    most_popular = mostPopular(g, limit)
+    printTop(most_popular)
 
+
+if __name__ == '__main__':
+    main()
 
 
 
