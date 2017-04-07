b50f5ca7de288048101a5c9aa0b6f2c106c15aec
TAG-748\Mikhail Belousov <belousoff2006@gmail.com>
2017-04-07 14:48:22 +0800

    added file encoding check

--- Netology25/find_procedure_v2.py
+++ Netology25/find_procedure_v2.py
@@ -7,12 +7,22 @@ Created on Sat Mar 18 19:22:55 2017
 
 import glob
 import os.path
+import chardet
 
+
+def get_encoding(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, "rb") as f:
+            encoding = chardet.detect(f.read())['encoding']
+        return encoding
+    else:
+        raise FileNotFoundError ('File not found')
 
 def search_key(files, check_smb):
     result_files = []
     for file in files:
-        for line in open(file, 'r'):
+        encoding = get_encoding(file)
+        for line in open(file, 'r', encoding=encoding):
             if check_smb in line:
                 result_files.append(file)
                 break
@@ -21,7 +31,7 @@ def search_key(files, check_smb):
 
 def main():
     home_dir = os.getcwd()
-    source_dir = 'Migrations'
+    source_dir = 'Advanced Migrations'
     file_list = glob.glob(os.path.join(home_dir, source_dir, "*.sql"))
     while True:
         check_smb = input("Введите строку\n>>")
