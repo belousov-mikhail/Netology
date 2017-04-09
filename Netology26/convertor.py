6d0220efd214a2b8abb4f1eb6351193dad86fec9
TAG-748\Mikhail Belousov <belousoff2006@gmail.com>
2017-04-09 16:19:15 +0800

    finished homework

---
+++ Netology26/convertor.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  9 13:21:25 2017
+
+@author: Mikhail Belousov
+"""
+import subprocess
+import os
+
+source_dir = os.path.join(os.getcwd(), 'Source')
+if not os.path.exists('Output'):
+    os.mkdir('Output')
+output_dir = os.path.join(os.getcwd(), 'Output')
+
+for file in os.listdir(source_dir):
+    if file.endswith('.jpg'):
+        source_file = os.path.join(source_dir, file)
+        result_file = os.path.join(output_dir, file)
+        command = 'convert.exe '+ source_file + ' -resize 200 ' + result_file
+        resize=subprocess.Popen(command)
+
+
+
+
